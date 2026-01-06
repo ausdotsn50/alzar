7a067925972a2ff0b74eb212fe0ae314ca792199
ausdotsn50 <ausdotsn50@gmail.com>
2026-01-06 22:03:29 +0800

    fix: database initialization bug

--- app/app/_layout.jsx
+++ app/app/_layout.jsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from "react";
+import { SQLiteProvider } from "expo-sqlite";
 import { Slot } from "expo-router";
-import { SQLiteProvider, openDatabaseAsync } from "expo-sqlite";
 import { initDB } from "@/database/initDB"; 
 import PageLoader from '@/components/PageLoader';
 
 export default function RootLayout() {
-  const [isDbReady, setIsDbReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // 1. Manually open and init the DB first
-        const db = await openDatabaseAsync("alzar.db");
-        await initDB(db);
-        
-        // 2. Signal that we are ready
-        setIsDbReady(true);
-      } catch (error) {
-        console.error("Database prep failed:", error);
-      }
-    }
-
-    prepare();
-  }, []); // Run only ONCE on mount
-
-  if (!isDbReady) {
-    return <PageLoader />;
-  }
-
   return (
-    <SQLiteProvider databaseName="alzar.db">
+    <SQLiteProvider 
+      databaseName="alzar.db" 
+      onInit={initDB}
+      loadingUi={<PageLoader />} 
+    >
       <Slot />
     </SQLiteProvider>
   );
